a9994910a4440950bdc4dd7ab85ebda652c3e40f
Shaheed Dewan <Shaheed Dewan>
2016-02-07 14:11:23 +0600

    Common Infos added

---
+++ src/common/ClientInfo.java
@@ -0,0 +1,51 @@
+/*
+ * Shaheed Ahmed Dewan Sagar
+ * AUST-12-01-04-085
+ * sdewan64@gmail.com
+ */
+
+package common;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ *
+ * @author Shaheed Ahmed Dewan Sagar
+ *         AUST-12.01.04.085
+ *         sdewan64@gmail.com
+ */
+public class ClientInfo {
+    
+    private final int id;
+    private final DataInputStream dataInputStream;
+    private final DataOutputStream dataOutputStream;
+    
+    private int currentPosition = 0;
+
+    public ClientInfo(int id, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
+        this.id = id;
+        this.dataInputStream = dataInputStream;
+        this.dataOutputStream = dataOutputStream;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public DataInputStream getDataInputStream() {
+        return dataInputStream;
+    }
+
+    public DataOutputStream getDataOutputStream() {
+        return dataOutputStream;
+    }
+    
+    public int getCurrentPosition(){
+        return currentPosition;
+    }
+    
+    public void setCurrentPosition(int number){
+        currentPosition = number;
+    }
+}
---
+++ src/common/CommonInformation.java
@@ -0,0 +1,24 @@
+/*
+ * Shaheed Ahmed Dewan Sagar
+ * AUST-12-01-04-085
+ * sdewan64@gmail.com
+ */
+
+package common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Shaheed Ahmed Dewan Sagar
+ *         AUST-12.01.04.085
+ *         sdewan64@gmail.com
+ */
+public class CommonInformation {
+    public static final String frameHeader = "Multiplayer Snake Ludo";
+    public static final String HOST = "localhost";
+    public static final int PORT = 8590;
+    
+    public static List<ClientInfo> clientInfos = new ArrayList<>();
+}
